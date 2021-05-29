d4faa47c9c52b1895358c5f116a96a806ac79ebf
SebastiaanYN <sebastiaanyn@gmail.com>
2021-05-29 18:59:25 +0200

    Initial commit

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ZSH_CUSTOM="$ZSH/custom"
+
+sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
